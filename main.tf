386a7962d4b773bb3e7bc7daa1ea40bed46e01c2
krishna95-teja <120112782+krishna95-teja@users.noreply.github.com>
2026-02-22 00:35:03 +0530

    Rename container to container_name in main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
  backend "azurerm" {
   resource_group_name = "rg-tf-backend"
   storage_account_name = "sttfbackend1"
-  container = "tfstate"
+  container_name = "tfstate"
   key = "dev.terraform.tfstate"       # Path to the statefile is sttfbackend1/tfstate/dev.terraform.tfstate
   }
  }
